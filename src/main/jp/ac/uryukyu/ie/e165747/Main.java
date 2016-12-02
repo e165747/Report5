357469919416c9bcb075bce98bf14fb5bb86922f
Kohei Kina <e165747@ie.u-ryukyu.ac.jp>
2016-12-02 14:11:23 +0900

    1st update Report5

---
+++ src/main/jp/ac/uryukyu/ie/e165747/Main.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e165747;
+
+/**
+ * Created by e165747 on 2016/12/02.
+ */
+public class Main {
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }
+}
